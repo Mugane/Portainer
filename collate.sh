ec1b8d89019122659b1d68ed32263d719dd74c00
Peter Kionga-Kamau <13005927+Mugane@users.noreply.github.com>
2024-07-29 22:36:58 -0400

    templates.json collation script updates & documentation

--- collate.sh
+++ collate.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-unlink ./templates.json
+# This script collates the individual .json templates in the ./templates folder into a single templates.json file
+# The output is published by GitHub Actions to GitHub Pages at https://mugane.github.io/Portainer/templates.json
+
+unlink ./templates.json 2>/dev/null # Qietly delete templates.json if it exists
 printf "{\n  \"version\": \"2\",\n  \"templates\": [\n" > ./templates.json
-for file in ./templates/*.json; do # collate all the json files in ./templates separated by a comma
-  sed -e 's/^/    /'  -e 's/\\n\\s*$//' "$file" >> ./templates.json # add spaces for formatting, remove trailing space/newlines
-  echo "," >> ./templates.json # separate each template with a comma
+for file in ./templates/*.json; do # Collate all the json files in ./templates separated by a comma
+  sed -e 's/^/    /'  -e 's/\\n\\s*$//' "$file" >> ./templates.json # Add spaces for formatting, remove trailing space/newlines
+  echo "," >> ./templates.json # Separate each joined template with a comma
 done
-# sed -i -z '$s/..$//' ./templates.json # take off the last comma (and ignore the last newline character) - this version doesn't work well on zsh
-sed -i '/,/{$d}' ./templates.json # take off the last comma (and ignore the last newline character)
-printf "}\n  ]\n}" >> ./templates.json # close the json object collection
+sed -i '/,/{$d}' ./templates.json # Remove the last comma (and any trailing spaces)
+printf "}\n  ]\n}" >> ./templates.json # Close the json object collection
 echo "./templates/ concatenated successfully into ./templates.json"
